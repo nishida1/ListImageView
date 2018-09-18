5f52934cb8804d3ce73690b8ac8286612917144e
nishida1 <nishidanexus6@gmail.com>
2018-09-18 16:55:16 +0900

    listimage

---
+++ app/src/main/java/com/example/nishida/listimageview/ListItem.java
@@ -0,0 +1,20 @@
+package com.example.nishida.listimageview;
+
+public class ListItem {
+
+    private int imageId;
+    private String text;
+
+    public int getImageId() {
+        return imageId;
+    }
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+    public String getText() {
+        return text;
+    }
+    public void setText(String text) {
+        this.text = text;
+    }
+}
--- app/src/main/java/com/example/nishida/listimageview/MainActivity.java
+++ app/src/main/java/com/example/nishida/listimageview/MainActivity.java
@@ -3,11 +3,40 @@ package com.example.nishida.listimageview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ListView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_list_view);
+
+        List<ListItem> list = new ArrayList<ListItem>();
+
+        for (int i = 1; i < 6; i++) {
+            ListItem item = new ListItem();
+            item.setText("アイテム\n" + i);
+
+            // TODO 画像ファイルをセット
+            //item.setImageId(R.drawable.ic_launcher);
+            list.add(item);
+        }
+
+        // adapterのインスタンスを作成
+        ImageArrayAdapter adapter =
+                new ImageArrayAdapter(this, R.layout.list_view_image_item, list);
+
+        lv = (ListView) findViewById(R.id.listview);
+        lv.setAdapter(adapter);
+
     }
+
 }
